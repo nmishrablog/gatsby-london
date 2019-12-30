3afbb1cd7a4b09a171ee303b7a878ef83737bc4f
nmishrablog <31518505+nmishrablog@users.noreply.github.com>
2019-12-30 14:43:43 +0530

    Update postCard.js

--- src/components/postCard.js
+++ src/components/postCard.js
@@ -6,13 +6,7 @@ export default props => (
     className={`post-card ${props.count % 3 === 0 && `post-card-large`} ${
       props.postClass
     } ${props.node.frontmatter.thumbnail ? `with-image` : `no-image`}`}
-    style={
-      props.node.frontmatter.thumbnail && {
-        backgroundImage: `url(${
-          props.node.frontmatter.thumbnail.childImageSharp.fluid.src
-        })`,
-      }
-    }
+ 
   >
     <Link to={props.node.fields.slug} className="post-card-link">
       <div className="post-card-content">
